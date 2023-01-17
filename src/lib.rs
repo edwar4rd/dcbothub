9025b44791ea2ff86fd5235afb64862a87d30da0
edwar4rd <edwar4rd@gmail.com>
2023-01-17 18:20:50 +0800

    add Bots BotInstances TaskType Tasks helper type

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,30 @@
 pub mod bot_parser;
 pub mod cmd_parser;
+
+#[derive(std::fmt::Debug)]
+pub enum TaskType {
+    Clean,
+    Build,
+    Pull,
+}
+
+impl std::fmt::Display for TaskType {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Self::Clean => "Clean",
+                Self::Build => "Build",
+                Self::Pull => "Pull",
+            }
+        )
+    }
+}
+
+pub type Bots = std::collections::HashMap<String, bot_parser::Bot>;
+pub type BotInstances = std::collections::HashMap<String, Result<std::process::Child, String>>;
+pub type Tasks = std::collections::HashMap<
+    String,
+    ((String, TaskType, u32), Result<std::process::Child, String>),
+>;
